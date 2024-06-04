ff19f1791a4ee2bf7af092b02e65bb73a8c85e31
KARTHICK-R <karthicknandha619@gmail.com>
2024-06-04 13:11:49 +0530

    modified changes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,160 @@
-import "./App.css";
 import { useState } from "react"
-
-
-//For using useState we need to import useState from react
+import { AddColor } from "./AddColor";
+import "./App.css";
 import { Counter } from "./Counter";
+import { Routes, Route, Link } from "react-router-dom"
+
+const INITIAL_MOVIE_LIST = [
+  {
+    name: "Brigerton",
+    poster: "http://image.tmdb.org/t/p/original//luoKpgVwi1E5nQsi7W0UuKHu2Rq.jpg",
+    rating: 4,
+    trailer: "https://youtu.be/gpv7ayf_tyE",
+    summary: "Bridgerton follows Daphne Bridgerton (Phoebe Dynevor), the eldest daughter of the powerful Bridgerton family as she makes her debut onto Regency London's competitive marriage market. Hoping to follow in her parent's footsteps and find Link match sparked by true love, Daphne's prospects initially seem to be unrivaled.",
+    cast: {
+      actor: "Nicola Coughlan",
+      actress: "Claudia Jessie"
+    },
+    duration: "60min"
+  },
+  {
+    name: "Lucifer",
+    poster: "http://image.tmdb.org/t/p/original//ekZobS8isE6mA53RAiGDG93hBxL.jpg",
+    rating: 4.7,
+    trailer: "https://youtu.be/X4bF_quwNtw",
+    summary: "Lucifer Morningstar (Tom Ellis) is the devil. He's tired of Hell and takes Link break in L.A. He's running his nightclub Lux with demon disciple Mazikeen (Lesley-Ann Brandt). His brother Amenadiel (D.B. Woodside) demands that he returns to Hell.",
+    cast: {
+      actor: "Tom Ellis",
+      actress: "Lauren German"
+    },
+    duration: "100min"
+  },
+  {
+    name: "Brigerton",
+    poster: "http://image.tmdb.org/t/p/original//luoKpgVwi1E5nQsi7W0UuKHu2Rq.jpg",
+    rating: 5,
+    trailer: "https://youtu.be/gpv7ayf_tyE",
+    summary: "Bridgerton follows Daphne Bridgerton (Phoebe Dynevor), the eldest daughter of the powerful Bridgerton family as she makes her debut onto Regency London's competitive marriage market. Hoping to follow in her parent's footsteps and find Link match sparked by true love, Daphne's prospects initially seem to be unrivaled.",
+    cast: {
+      actor: "Nicola Coughlan",
+      actress: "Claudia Jessie"
+    },
+    duration: "60min"
+  },
+  {
+    name: "Lucifer",
+    poster: "http://image.tmdb.org/t/p/original//ekZobS8isE6mA53RAiGDG93hBxL.jpg",
+    rating: 3.5,
+    trailer: "https://youtu.be/X4bF_quwNtw",
+    summary: "Lucifer Morningstar (Tom Ellis) is the devil. He's tired of Hell and takes Link break in L.A. He's running his nightclub Lux with demon disciple Mazikeen (Lesley-Ann Brandt). His brother Amenadiel (D.B. Woodside) demands that he returns to Hell.",
+    cast: {
+      actor: "Tom Ellis",
+      actress: "Lauren German"
+    },
+    duration: "100min"
+  },
+  {
+    name: "Brigerton",
+    poster: "http://image.tmdb.org/t/p/original//luoKpgVwi1E5nQsi7W0UuKHu2Rq.jpg",
+    rating: 4.6,
+    trailer: "https://youtu.be/gpv7ayf_tyE",
+    summary: "Bridgerton follows Daphne Bridgerton (Phoebe Dynevor), the eldest daughter of the powerful Bridgerton family as she makes her debut onto Regency London's competitive marriage market. Hoping to follow in her parent's footsteps and find Link match sparked by true love, Daphne's prospects initially seem to be unrivaled.",
+    cast: {
+      actor: "Nicola Coughlan",
+      actress: "Claudia Jessie"
+    },
+    duration: "60min"
+  },
+  {
+    name: "Lucifer",
+    poster: "http://image.tmdb.org/t/p/original//ekZobS8isE6mA53RAiGDG93hBxL.jpg",
+    rating: 4,
+    trailer: "https://youtu.be/X4bF_quwNtw",
+    summary: "Lucifer Morningstar (Tom Ellis) is the devil. He's tired of Hell and takes Link break in L.A. He's running his nightclub Lux with demon disciple Mazikeen (Lesley-Ann Brandt). His brother Amenadiel (D.B. Woodside) demands that he returns to Hell.",
+    cast: {
+      actor: "Tom Ellis",
+      actress: "Lauren German"
+    },
+    duration: "100min"
+  }
+
+]
+
 
 export default function App() {
+
   return (
     <div className="App">
-      <AddColor />
+      <nav>
+        <ul>
+          {/* Link change page without refresh */}
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/movies">MovieList</Link></li>
+          <li><Link to="/add-color">AddColor</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<MovieList />} />
+        <Route path="/add-color" element={<AddColor />} />
+      </Routes>
     </div>
   );
+  //JSX ends
 }
 
-function AddColor() {
-  const [color, setColor] = useState("skyblue")
-  const [colorList, setColorList] = useState(["orange", "crimson", "pink"])
 
-  const inputStyles = {
-    fontSize: "25px",
-    backgroundColor: color,
-  };
+function Home() {
+  const headerStyle ={
+      textAlign:"center",
+      backgroundColor: "rgb(111, 53, 111)",
+      color:"white",
+  }
 
   return (
-    <div>
-      <div className="add-color">
-        <input
-          style={inputStyles}
-          type="text"
-          value={color}
-          onChange={(event) => setColor(event.target.value)}
-        />
-        <button onClick={()=> setColorList([...colorList, color])}>Add Color</button>
-      </div>
-      {colorList.map((ele) => (
-        <ColorBox color={ele} />
-      ))}
-    </div>
-  );
+    <h1 style={headerStyle}>Welcome to Netflix App</h1>
+  )
 }
 
-function ColorBox({ color }) {
-  const styles = {
-    width: "315px",
-    height: "30px",
-    backgroundColor: color,
-    marginTop:"15px",
-  };
-  return <div style={styles}></div>;
+function MovieList() {
+  const movieList = INITIAL_MOVIE_LIST
+  return (
+    <div className="movie-list">
+      {movieList.map((mv) => (
+        <Movie movie={mv} />
+      ))}
+    </div>
+  )
+}
+
+
+function Movie({ movie }) {
+  console.log(movie.poster)
+
+  const [show, setShow] = useState(true)
+
+  const ratingStyles = {
+    color: movie.rating <= 4.5 ? "red" : "green"
+  }
+
+  // const summaryStyle = {
+  //   display: show ? "block" : "none"
+  // }
+  return (
+    <div className="movie-card">
+      <img className="movie-poster" src={movie.poster} alt={movie.name} />
+      <div className="movie-spec">
+        <h2>{movie.name}</h2>
+        <h3 style={ratingStyles}>⭐{movie.rating}</h3>
+      </div>
+      <button onClick={() => setShow(!show)} >Toggle description</button>
+      {/* <p style={summaryStyle} className="movie-summary">{movie.summary}</p> */}
+      {show ? <p className="movie-summary">{movie.summary}</p> : ""}
+      <div className="movie-footer">
+        <Counter />
+        <button className="movie-button">Watch Now</button>
+      </div>
+    </div>
+  )
 }
--- src/Movies.js
+++ src/Movies.js
@@ -1,13 +1,13 @@
-// Function for adding Movies as array of Object
-function Movies(props) {
-  return (
-    <div className="movie-container">
-      <img className="movie-pic" src={props.poster} alt={props.name} />
-      <div className="movie-info">
-        <p>Movie Name: {props.name}</p>
-        <p>Rating: {props.rating} </p>
-        <p>Summary: {props.summary}</p>
-      </div>
-    </div>
-  );
-}
+// // Function for adding Movies as array of Object
+// function Movies(props) {
+//   return (
+//     <div className="movie-container">
+//       <img className="movie-pic" src={props.poster} alt={props.name} />
+//       <div className="movie-info">
+//         <p>Movie Name: {props.name}</p>
+//         <p>Rating: {props.rating} </p>
+//         <p>Summary: {props.summary}</p>
+//       </div>
+//     </div>
+//   );
+// }
