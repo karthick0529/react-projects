50e17e937d5afc983f03f1ccc969ff11046e5e76
KARTHICK-R <karthicknandha619@gmail.com>
2024-06-05 12:09:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -269,6 +269,7 @@ export default function App() {
         <Route path="/add-color" element={<AddColor />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      
     </div>
   );
   //JSX ends
