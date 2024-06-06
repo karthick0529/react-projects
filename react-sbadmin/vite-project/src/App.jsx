042bd5111c6a289932b10135fe0ea051a25543d6
KARTHICK-R <karthicknandha619@gmail.com>
2024-06-06 18:17:30 +0530

    updated code

--- react-sbadmin/vite-project/src/App.jsx
+++ react-sbadmin/vite-project/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <Topbar />
+            <div className="container-fluid">
+              <Switch>
+                <Route exact={true} path="/" component={Dashboard} />
+              </Switch>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ react-sbadmin/vite-project/src/components/Card.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState } from "react";
+
+function Card() {
+  const [show, setShow] = useState(true);
+  const data = [
+    {
+      color: "primary",
+      text: "Earnings (Monthly)",
+      amount: "$40,000",
+      icon: "fas fa-calendar",
+    },
+    {
+      color: "success",
+      text: "Earnings (Annual)",
+      amount: "$215,000",
+      icon: "fas fa-dollar-sign",
+    },
+    {
+      color: "info",
+      text: "Tasks",
+      amount: "50%",
+      icon: "fas fa-clipboard-list",
+    },
+    {
+      color: "warning",
+      text: "Pending Requests",
+      amount: "18",
+      icon: "fas fa-comments",
+    },
+  ];
+
+  return (
+    <div className="row">
+      {data.map((item, index) => {
+        return (
+          <div key={index} className="col-xl-3 col-md-6 mb-4">
+            <div className={`card border-left-${item.color} shadow h-100 py-2`}>
+              <div className="card-body">
+                <div className="row no-gutters align-items-center">
+                  <div className="col mr-2">
+                    <div
+                      className={`text-xs font-weight-bold text-${item.color} text-uppercase mb-1`}
+                    >
+                      {item.text}
+                    </div>
+                    <div className="d-flex align-items-center">
+                    <div className="h5 mb-0 font-weight-bold text-gray-800">
+                      {item.amount}
+                    </div>
+                    {item.text === "Tasks" && show && (
+                      <div className="progress progress-sm ml-2 flex-grow-1">
+                        <div
+                          className="progress-bar bg-info"
+                          role="progressbar"
+                          style={{ width: "50%" }}
+                          aria-valuenow="50"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        ></div>
+                      </div>
+                    )}
+                  </div>
+                  </div>
+                  <div className="col-auto">
+                    <i className={`fas ${item.icon} fa-2x text-gray-300`}></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Card;
