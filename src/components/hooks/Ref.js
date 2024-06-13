26d3e72817c8d59e4c5d93aa7f1da1b18bdc3dfa
KARTHICK-R <karthicknandha619@gmail.com>
2024-06-13 12:44:00 +0530

    updated wth REF

--- src/components/hooks/Ref.js
+++ src/components/hooks/Ref.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
 function Ref() {
+
+  const inputRef = useRef()
+
+  const handleInputChange = () => {
+    console.log("handleInputChange", inputRef.current.value)
+  }
+
+  const handleSubmit = () => {
+    console.log("handleSubmit", inputRef.current.value)
+  }
+
   return (
-    <div>Ref</div>
+    <div>
+            <h1>Ref Example</h1>
+            <input type="text" ref={inputRef} onChange={handleInputChange} />
+            <button onClick={handleSubmit}>Get Value</button> 
+            {/* <p>{inputRef.current.value}</p> */}
+        </div>
   )
 }
 
