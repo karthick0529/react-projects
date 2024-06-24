69e95595965b07548d5ccbe5e11e7684b95b8bc8
KARTHICK-R <karthicknandha619@gmail.com>
2024-06-24 12:14:04 +0530

    modified

--- axios-task/src/App.jsx
+++ axios-task/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AppRoutes from './components/AppRoutes'
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
-export const API_URL='https://659848cf668d248edf2466c4.mockapi.io/taskblog'
+export const API_URL='https://664db4f9ede9a2b5565481c8.mockapi.io/axios'
 
 
 function App() {
