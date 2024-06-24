895bcb9fa67c772973ef82903413829a79a2593a
KARTHICK-R <karthicknandha619@gmail.com>
2024-06-24 15:08:14 +0530

    updated

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -8,7 +8,10 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 
 const moviesValidationSchema = yup.object({
-  name: yup.string().min(5, "Enter the name").required("Name is required"),
+  name: yup
+    .string()
+    .min(5, "Enter the name")
+    .required("Name is required"),
   poster: yup
     .string()
     .min(20, "Enter the poster")
@@ -26,27 +29,27 @@ const moviesValidationSchema = yup.object({
 });
 
 function AddMovie() {
+    
+    // const [name, setName] = useState("")
+    // const [poster, setPoster] = useState("")
+    // const [rating, setRating] = useState()
+    // const [summary, setSummary] = useState("")
+    
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            poster: "",
+            rating: "",
+            summary: ""
+        },
+        validationSchema: moviesValidationSchema,
+        onSubmit: (newMovie) => {
+            console.log("onSubmit", newMovie);
+            createMovies(newMovie);
+        },
+    });
+
   const navigate = useNavigate();
-
-  // const [name, setName] = useState("")
-  // const [poster, setPoster] = useState("")
-  // const [rating, setRating] = useState()
-  // const [summary, setSummary] = useState("")
-
-  const formik = useFormik({
-    initialValues: {
-      name: "Gravity",
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/f/f6/Gravity_Poster.jpg",
-      rating: "5",
-      summary:
-        "Dr. Ryan Stone (Sandra Bullock) is a medical engineer on her first shuttle mission. Her commander is veteran astronaut Matt Kowalsky (George Clooney)",
-    },
-    validationSchema: moviesValidationSchema,
-    onSubmit: (newMovie) => {
-      console.log("onSubmit", newMovie);
-    },
-  });
 
   const createMovies = (newMovie) => {
     console.log("newMovie", newMovie);
@@ -69,6 +72,7 @@ function AddMovie() {
         id="name"
         label="Name"
         variant="outlined"
+        name="name"
         // value={name}
         // onChange={(event) => setName(event.target.value)}
         value={formik.values.name}
@@ -82,6 +86,7 @@ function AddMovie() {
         id="poster"
         label="Poster"
         variant="outlined"
+        name="poster"
         // value={poster}
         // onChange={(event) => setPoster(event.target.value)}
         value={formik.values.poster}
@@ -95,6 +100,7 @@ function AddMovie() {
         id="rating"
         label="Rating"
         variant="outlined"
+        name="rating"
         // value={rating}
         // onChange={(event) => setRating(event.target.value)}
         value={formik.values.rating}
@@ -108,6 +114,7 @@ function AddMovie() {
         id="summary"
         label="Summary"
         variant="outlined"
+        name="summary"
         // value={summary}
         // onChange={(event) => setSummary(event.target.value)}
         value={formik.values.summary}
@@ -122,7 +129,7 @@ function AddMovie() {
         variant="contained"
         type="submit"
         onClick={createMovies}
-        // ={() => {
+        // onClick={() => {
         //   const newMovie = {
         //     name,
         //     poster,
